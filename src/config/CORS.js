1f21161896632ed4882dddac7855167cf0bd0eb2
Toan <nguyentoan04.0003@gmail.com>
2023-11-12 14:53:28 +0700

    Compete permission

--- src/config/CORS.js
+++ src/config/CORS.js
@@ -1,35 +1,33 @@
 require("dotenv").config();
+
 const configCors = (app) => {
+  // Add headers before the routes are defined
   app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+
     // Request methods you wish to allow
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
     );
+
     // Request headers you wish to allow
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
+      "X-Requested-With,content-type, Authorization"
     );
+
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader("Access-Control-Allow-Credentials", true);
-    // Pass to next layer of middleware
-    // Cấu hình cho yêu cầu OPTIONS
 
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    // Pass to next layer of middleware
     next();
   });
-
-  app.options(
-    ["/api/v1/user/update", "/api/v1/user/create", "/api/v1/user/delete"],
-    function (req, res) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      res.status(200).send();
-    }
-  );
 };
+
 export default configCors;
